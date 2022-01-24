96bb89c27af525a4b84ea015f57550af976057dd
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-01-24 14:10:20 -0500

    up to date with jannuze

--- server.ts
+++ server.ts
@@ -3,9 +3,9 @@ const app = express();
 
 app.get("/hello", (req, res) => res.send("Hello World!"));
 
-app.get("/add/:a/:b", (req, res) => {
+app.get('/add/:a/:b', (req, res) => {
     res.send(req.params.a + req.params.b);
-});
+})
 
-const PORT =4000;
-app.listen( process.env.PORT || PORT);
+const PORT = 4000;
+app.listen(process.env.PORT || PORT);
