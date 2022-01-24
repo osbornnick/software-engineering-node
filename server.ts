ab4b720fd8056e122a48143d9019da67803fb65b
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-01-24 14:07:46 -0500

    update server with add

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
-app.get('/hello', (req, res) =>
-    res.send('Hello World!'));
+app.get("/hello", (req, res) => res.send("Hello World!"));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT);
+app.get("/add/:a/:b", (req, res) => {
+    res.send(req.params.a + req.params.b);
+});
+
+const PORT =4000;
+app.listen( process.env.PORT || PORT);
