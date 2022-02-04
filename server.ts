ec99b7324f804fffded701fad147f13b5d323e9f
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-02-04 15:40:08 -0500

    mongoose now connects to mongo atlas, no test env

--- server.ts
+++ server.ts
@@ -5,8 +5,9 @@ import UserController from "./controllers/UserController";
 import TuitDao from "./daos/TuitDao";
 import UserDao from "./daos/UserDao";
 
-if (process.env.NODE_ENV === "test")
-    mongoose.connect("mongodb://localhost:27017/tuiter");
+mongoose.connect(
+    "mongodb+srv://admin:tKSu4A4ScgtEuCpF@cluster0.hejjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
 
 const app = express();
 
@@ -18,14 +19,8 @@ app.get("/add/:a/:b", (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b)
 );
 
-if (process.env.NODE_ENV === "test") {
-    const tuitController = new TuitController(
-        app,
-        new TuitDao(),
-        new UserDao()
-    );
-    const userController = new UserController(app, new UserDao());
-}
+const tuitController = new TuitController(app, new TuitDao(), new UserDao());
+const userController = new UserController(app, new UserDao());
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
