442c7e9b21366a42f359b8b19fc3e9cc20202a5f
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-02-04 16:28:02 -0500

    use urlencoded for parsing

--- server.ts
+++ server.ts
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/hello", (req: Request, res: Response) => res.send("Hello World!"));
 
