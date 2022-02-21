fa2bbc3aa89d6668e11491ad90d481931bee0d7f
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-02-21 10:36:23 -0500

    merge daos

--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import TuitDaoI from "../interfaces/TuitDao";
-import Tuit from "../models/Tuit";
-import TuitModel from "../mongoose/TuitModel";
-
-export default class TuitDao implements TuitDaoI {
-    private static tuitDao: TuitDao | null = null;
-    public static getInstance = (): TuitDao => {
-        if (TuitDao.tuitDao === null) TuitDao.tuitDao = new TuitDao();
-        return TuitDao.tuitDao;
-    };
-
-    private constructor() {}
-    async findAllTuits(): Promise<Tuit[]> {
-        return await TuitModel.find();
-    }
-
-    findTuitsByUser = async (uid: string): Promise<Tuit[]> =>
-        TuitModel.find({ postedBy: uid });
-
-    findTuitById = async (tid: string): Promise<Tuit> =>
-        TuitModel.findById(tid);
-
-    async createTuit(tuit: Tuit): Promise<Tuit> {
-        return await TuitModel.create(tuit);
-    }
-    async updateTuit(tid: string, tuit: Tuit): Promise<any> {
-        return await TuitModel.updateOne({ _id: tid }, { $set: tuit });
-    }
-    async deleteTuit(tid: string): Promise<any> {
-        return await TuitModel.deleteOne({ _id: tid });
-    }
-}
-=======
 /**
  * @file Implements DAO managing data storage of tuits. Uses mongoose TuitModel
  * to integrate with MongoDB
@@ -71,4 +37,3 @@ export default class TuitDao implements TuitDaoI{
     deleteTuit = async (uid: string): Promise<any> =>
         TuitModel.deleteOne({_id: uid});
 }
->>>>>>> aaf52270036fe5e6fc05f9de70b05b4797961be1
--- daos/UserDao.ts
+++ daos/UserDao.ts
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import User from "../models/User";
-import UserModel from "../mongoose/UserModel";
-import UserDaoI from "../interfaces/UserDao";
-
-export default class UserDao implements UserDaoI {
-    private static userDao: UserDao | null = null;
-    public static getInstance = (): UserDao => {
-        if (UserDao.userDao === null) {
-            UserDao.userDao = new UserDao();
-        }
-        return UserDao.userDao;
-    };
-    private constructor() {}
-    async findAllUsers(): Promise<User[]> {
-        return await UserModel.find();
-    }
-    async findUserById(uid: string): Promise<User> {
-        return await UserModel.findById(uid);
-    }
-    async createUser(user: User): Promise<User> {
-        return await UserModel.create(user);
-    }
-    async deleteUser(uid: string): Promise<any> {
-        return await UserModel.deleteOne({ _id: uid });
-    }
-    async updateUser(uid: string, user: User): Promise<any> {
-        return await UserModel.updateOne({ _id: uid }, { $set: user });
-    }
-}
-=======
 /**
  * @file Implements DAO managing data storage of users. Uses mongoose UserModel
  * to integrate with MongoDB
@@ -120,4 +89,3 @@ export default class UserDao implements UserDaoI {
     findUserByUsername = async (username: string): Promise<any> =>
         UserModel.findOne({username});
 };
->>>>>>> aaf52270036fe5e6fc05f9de70b05b4797961be1
