441f0207950f846a12c394f6a3a7b42c639dd22c
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-02-21 11:15:11 -0500

    fix postinstall

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ import mongoose from "mongoose";
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
+const HOST = "cluster0.hejjh.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
